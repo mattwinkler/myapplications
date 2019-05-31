d5dd2f4220ef9ed93977240a0e8be459747cd481
mattwinkler <mwinkler1419@gmail.com>
2019-05-30 21:54:13 -0500

    add fibonnaci

--- algorithms/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ algorithms/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -5,5 +5,17 @@ def calc_fib(n):
 
     return calc_fib(n - 1) + calc_fib(n - 2)
 
+def calc_fib_fast(n):
+    if (n <= 1):
+        return n
+    
+    else:
+        seq_list = list(range(n))
+        for i in range(2, n):
+            seq_list[i] = seq_list[(i - 1)] + seq_list[(i - 2)]
+    
+        return seq_list[-1] + seq_list[-2]
+
+
 n = int(input())
-print(calc_fib(n))
+print(calc_fib_fast(n))
