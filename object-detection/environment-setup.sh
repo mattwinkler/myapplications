a22592d1f0d12ba15cb00a106b79094e8c6d93c1
mattwinkler <mwinkler1419@gmail.com>
2020-04-19 14:11:53 -0500

    add environment setup

--- object-detection/environment-setup.sh
+++ object-detection/environment-setup.sh
@@ -1,6 +1,11 @@
+# create and activate virtual environment
 virtualenv venv
 source venv/bin/activate
+
+# install dependencies
 pip install -r requirements.txt
+
+# make sure jupyter can see the installed venv
 pip install --user ipykernel
 python -m ipykernel install --user --name=venv
 
